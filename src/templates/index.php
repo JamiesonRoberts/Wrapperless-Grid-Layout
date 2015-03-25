543b184363b49abe3b297d0ac13a97d3929dc2b0
Jamieson Roberts <hello@jamiesonroberts.ca>
2015-03-25 00:04:06 -0400

    Working on the main section content setup to deal with all layouts and types

--- src/templates/index.php
+++ src/templates/index.php
@@ -16,5 +16,23 @@
         <h6>Heading</h6>
         <p>Habitasse sed gravida scelerisque hac nisi ligula scelerisque accumsan conubia suspendisse venenatis eleifend tristique egestas sagittis ullamcorper at tempus pretium. Conubia convallis gravida vel suspendisse mattis molestie sit luctus non vitae dignissim eu magnis a torquent litora nisl tincidunt felis parturient enim a in adipiscing sit. Vestibulum tempor maecenas per at eu praesent ullamcorper cum ut nibh taciti feugiat adipiscing a ullamcorper ridiculus vehicula quis rhoncus viverra.</p>
     </section>
+
+        <h2>Sectionless Content</h2> 
+        <p>Parturient suspendisse vestibulum semper cum nibh orci condimentum a sapien tempor sit primis at cras dolor suspendisse a quam consectetur nibh habitant euismod. Cubilia etiam faucibus dui parturient semper morbi amet nibh litora vulputate dictum elit sit habitasse vestibulum dictum euismod ullamcorper leo fames habitant quam ut parturient parturient. Metus fermentum mi ante metus a fermentum ultricies natoque ultrices sit parturient ut ornare condimentum habitasse ultricies nec eget aliquet mus. Condimentum duis porta a nibh elit iaculis vel vestibulum parturient vulputate mi a a natoque consequat scelerisque sociosqu consectetur cras quisque vestibulum. In platea mauris vivamus odio a facilisis tempor a a at torquent vestibulum dapibus commodo scelerisque torquent condimentum parturient malesuada diam etiam dictumst faucibus porttitor nam hac sociis himenaeos.</p>
+
+    <section>
+        <h2>Heading</h2>
+        <p>Parturient suspendisse vestibulum semper cum nibh orci condimentum a sapien tempor sit primis at cras dolor suspendisse a quam consectetur nibh habitant euismod. Cubilia etiam faucibus dui parturient semper morbi amet nibh litora vulputate dictum elit sit habitasse vestibulum dictum euismod ullamcorper leo fames habitant quam ut parturient parturient. Metus fermentum mi ante metus a fermentum ultricies natoque ultrices sit parturient ut ornare condimentum habitasse ultricies nec eget aliquet mus. Condimentum duis porta a nibh elit iaculis vel vestibulum parturient vulputate mi a a natoque consequat scelerisque sociosqu consectetur cras quisque vestibulum. In platea mauris vivamus odio a facilisis tempor a a at torquent vestibulum dapibus commodo scelerisque torquent condimentum parturient malesuada diam etiam dictumst faucibus porttitor nam hac sociis himenaeos.</p>
+
+        <h3>Heading</h3>
+        <p>Fusce orci orci eu taciti gravida suscipit a a mus a per id parturient conubia ullamcorper scelerisque odio vivamus nullam in. Felis a ultricies ligula amet rhoncus ante suspendisse id vel himenaeos adipiscing pretium varius aptent. Lacinia a eleifend a sit varius adipiscing ullamcorper eros parturient suspendisse conubia at leo lacinia at fermentum lacus eu sem ultrices. Velit himenaeos iaculis lectus eu purus consectetur adipiscing facilisis a parturient a scelerisque auctor taciti class iaculis adipiscing a.</p>
+    </section>
+    <section>
+        <h2>Heading</h2>
+        <p>Parturient suspendisse vestibulum semper cum nibh orci condimentum a sapien tempor sit primis at cras dolor suspendisse a quam consectetur nibh habitant euismod. Cubilia etiam faucibus dui parturient semper morbi amet nibh litora vulputate dictum elit sit habitasse vestibulum dictum euismod ullamcorper leo fames habitant quam ut parturient parturient. Metus fermentum mi ante metus a fermentum ultricies natoque ultrices sit parturient ut ornare condimentum habitasse ultricies nec eget aliquet mus. Condimentum duis porta a nibh elit iaculis vel vestibulum parturient vulputate mi a a natoque consequat scelerisque sociosqu consectetur cras quisque vestibulum. In platea mauris vivamus odio a facilisis tempor a a at torquent vestibulum dapibus commodo scelerisque torquent condimentum parturient malesuada diam etiam dictumst faucibus porttitor nam hac sociis himenaeos.</p>
+
+        <h3>Heading</h3>
+        <p>Fusce orci orci eu taciti gravida suscipit a a mus a per id parturient conubia ullamcorper scelerisque odio vivamus nullam in. Felis a ultricies ligula amet rhoncus ante suspendisse id vel himenaeos adipiscing pretium varius aptent. Lacinia a eleifend a sit varius adipiscing ullamcorper eros parturient suspendisse conubia at leo lacinia at fermentum lacus eu sem ultrices. Velit himenaeos iaculis lectus eu purus consectetur adipiscing facilisis a parturient a scelerisque auctor taciti class iaculis adipiscing a.</p>
+    </section>
 </main>
 <%= includes.footer %>
