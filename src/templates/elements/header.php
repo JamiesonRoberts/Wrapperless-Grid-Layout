cb00505a2b1488682a67362f001862750df045a3
Jamieson Roberts <hello@jamiesonroberts.ca>
2015-03-25 00:22:48 -0400

    correcting box model calculation for widths

--- src/templates/elements/header.php
+++ src/templates/elements/header.php
@@ -11,7 +11,7 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
         <header>
-            <h1><a href="#">Site title</a></h1>
+            <h1>Site title</h1>
             <nav class="mainnav nav-collapse">
                 <h2>Navigation</h2>
                 <ul>
