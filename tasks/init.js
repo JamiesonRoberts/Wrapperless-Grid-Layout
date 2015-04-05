1f4d097da89f735a8a7ed376ba15e72318dac361
Jamieson Roberts <hello@jamiesonroberts.ca>
2015-04-05 15:32:00 -0400

    cleaning out the files from the repo for the github page

--- tasks/init.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = function (gulp, plugins, config) {
-  config.gulp.tasks['scripts'](gulp, plugins, config);
-  config.gulp.tasks['images'](gulp, plugins, config);
-  config.gulp.tasks['fonts'](gulp, plugins, config);
-  config.gulp.tasks['icons'](gulp, plugins, config);
-  config.gulp.tasks['styles'](gulp, plugins, config);
-  config.gulp.tasks['templates'](gulp, plugins, config);
-};
